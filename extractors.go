b97cf45560c193823fbeae3f03feafa3b52c27e5
DH <adlandh@gmail.com>
2023-12-05 17:08:30 +0100

    add span_op for sentry extractor, span instead of transaction

--- extractors.go
+++ extractors.go
@@ -45,16 +45,17 @@ func WithValueExtractor(key ...fmt.Stringer) ContextExtractor {
 
 func WithSentryExtractor() ContextExtractor {
 	return func(ctx context.Context) []zap.Field {
-		transaction := sentry.TransactionFromContext(ctx)
-		if transaction == nil {
+		span := sentry.SpanFromContext(ctx)
+		if span == nil {
 			return nil
 		}
 
-		fields := make([]zap.Field, 3)
+		fields := make([]zap.Field, 4)
 
-		fields[0] = zap.String("trace_id", transaction.TraceID.String())
-		fields[1] = zap.String("span_id", transaction.SpanID.String())
-		fields[2] = zap.String("span_status", transaction.Status.String())
+		fields[0] = zap.String("trace_id", span.TraceID.String())
+		fields[1] = zap.String("span_id", span.SpanID.String())
+		fields[2] = zap.String("span_status", span.Status.String())
+		fields[3] = zap.String("span_op", span.Op)
 
 		return fields
 	}
